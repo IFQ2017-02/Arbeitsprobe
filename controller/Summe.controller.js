307bac3d5879124714aa5d61f97b800621af90c3
IFQ2017-02 <ifq2017-02@cloud.bs-hassfurt.de>
2019-07-19 13:36:47 +0200

    Änderungen Layout + Summe

--- controller/Summe.controller.js
+++ controller/Summe.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
           var oBundle = this.getView().getModel("i18n").getResourceBundle();
           var sValue1 = this.getView().getModel().getProperty("/summe/value1");
           var sValue2 = this.getView().getModel().getProperty("/summe/value2");
-          var sSumme = sValue1 + sValue2;
+          var sSumme = parsfloat(sValue1) + parsfloat(sValue2);
           var sMsg = oBundle.getText("ergebnisMsg", [sSumme]);
           // show message
           MessageToast.show(sMsg);
